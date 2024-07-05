bb7c2cfc4a39eb78f9dc5eb5a8bb35d371244b33
Vaibhav <vaibhav.sharma3100@gmail.com>
2024-07-05 21:06:22 +0530

    Implementing Creating Token and Account

--- src/MintToken.tsx
+++ src/MintToken.tsx
@@ -1,33 +1,66 @@
-import { clusterApiUrl, 
+import { 
+    clusterApiUrl, 
     Connection, 
     PublicKey, 
     Keypair, 
-    LAMPORTS_PER_SOL, 
-    Transaction, 
-    sendAndConfirmTransaction 
+    LAMPORTS_PER_SOL 
 } from '@solana/web3.js';
 import { 
     createMint, 
     getOrCreateAssociatedTokenAccount, 
     mintTo, 
+    transfer, 
     Account, 
-    createSetAuthorityInstruction, 
-    AuthorityType
+    getMint, 
+    getAccount
 } from '@solana/spl-token';
 
-const MintToken = () => {
-  return (
-    <div>
-        Mint Token Section 
+// Special setup to add a Buffer class, because it's missing
+window.Buffer = window.Buffer || require("buffer").Buffer;
+
+function MintToken() {
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+    // Generate a new wallet keypair and airdrop SOL
+    const fromWallet = Keypair.generate();
+    // Public Key to your Phantom Wallet
+    const toWallet = new PublicKey("INSERT YOUR PUBLIC KEY HERE");
+	let fromTokenAccount: Account; 
+	let mint: PublicKey;
+
+    async function createToken() {
+        const fromAirdropSignature = await connection.requestAirdrop(fromWallet.publicKey, LAMPORTS_PER_SOL);
+        await connection.confirmTransaction(fromAirdropSignature);
+    
+        // Create new token mint
+        mint = await createMint(
+            connection, 
+            fromWallet, 
+            fromWallet.publicKey, 
+            null, 
+            9 // 9 here means we have a decmial of 9 0's
+        );
+        console.log(`Create token: ${mint.toBase58()}`);
+    
+        // Get the token account of the fromWallet address, and if it does not exist, create it
+        fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+            connection,
+            fromWallet,
+            mint,
+            fromWallet.publicKey
+        );
+        console.log(`Create Token Account: ${fromTokenAccount.address.toBase58()}`);
+    }
+    return (
         <div>
-            <button>Create Token</button>
-            <button>MintToken</button>
-            <button>Check balance</button>
-            <button>Send Token</button>
-      </div>
-    </div>
-  )
+            Mint Token Section
+            <div>
+                <button onClick={createToken}>Create token</button>
+                <button >Mint token</button>
+                <button >Check balance</button>
+                <button >Send token</button>
+            </div>
+        </div>
+    );
 }
 
-export default MintToken
-
+export default MintToken;
