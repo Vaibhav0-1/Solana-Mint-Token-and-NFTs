377b4eb8a4e46de20e9a72335cae7dc5e576cdcd
Vaibhav <vaibhav.sharma3100@gmail.com>
2024-07-05 21:58:13 +0530

    Implementing balance and sending token

--- src/MintToken.tsx
+++ src/MintToken.tsx
@@ -14,24 +14,28 @@ import {
     getMint, 
     getAccount
 } from '@solana/spl-token';
+import { Buffer } from 'buffer';
 
 // Special setup to add a Buffer class, because it's missing
-window.Buffer = window.Buffer || require("buffer").Buffer;
+window.Buffer = window.Buffer ||  Buffer;
 
 function MintToken() {
     const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
     // Generate a new wallet keypair and airdrop SOL
     const fromWallet = Keypair.generate();
     // Public Key to your Phantom Wallet
-    const toWallet = new PublicKey("INSERT YOUR PUBLIC KEY HERE");
+    const toWallet = new PublicKey("");
 	let fromTokenAccount: Account; 
 	let mint: PublicKey;
 
     async function createToken() {
         const fromAirdropSignature = await connection.requestAirdrop(fromWallet.publicKey, LAMPORTS_PER_SOL);
-        await connection.confirmTransaction(fromAirdropSignature);
+        await connection.confirmTransaction({
+            signature: fromAirdropSignature,
+            blockhash: (await connection.getLatestBlockhash('finalized')).blockhash,
+            lastValidBlockHeight: (await connection.getLatestBlockhash('finalized')).lastValidBlockHeight,
+          }, 'confirmed');
     
-        // Create new token mint
         mint = await createMint(
             connection, 
             fromWallet, 
@@ -64,13 +68,39 @@ function MintToken() {
         console.log(`Mint signature: ${signature}`);
     }
 
+    async function checkBalance() {
+        // get the supply of tokens we have minted into existance
+        const mintInfo = await getMint(connection, mint);
+		console.log(mintInfo.supply);
+		
+		// get the amount of tokens left in the account
+        const tokenAccountInfo = await getAccount(connection, fromTokenAccount.address);
+		console.log(tokenAccountInfo.amount);
+    }
+
+    async function sendToken() {
+        // Get the token account of the toWallet address, and if it does not exist, create it
+        const toTokenAccount = await getOrCreateAssociatedTokenAccount(connection, fromWallet, mint, toWallet);
+        console.log(`toTokenAccount ${toTokenAccount.address}`);
+
+        const signature = await transfer(
+            connection,
+            fromWallet,
+            fromTokenAccount.address,
+            toTokenAccount.address,
+            fromWallet.publicKey,
+            1000000000 // 1 billion
+        );
+        console.log(`finished transfer with ${signature}`);
+    }
+
     return (
         <div>
             Mint Token Section
             <div>
                 <button onClick={createToken}>Create token</button>
                 <button onClick={mintToken}>Mint token</button>
-                <button >Check balance</button>
+                <button onClick={checkBalance}>Check balance</button>
                 <button >Send token</button>
             </div>
         </div>
